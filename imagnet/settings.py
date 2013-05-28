20d4db00b0d639d1cd5ecb76f556ad68203cee72
angry_freelancer <eugenolshevsky@gmail.com>
2013-05-28 04:07:27 +0300

    change settings

--- imagnet/settings.py
+++ imagnet/settings.py
@@ -2,7 +2,7 @@
 import os
 from django.utils.translation import ugettext_lazy as gettext
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -59,8 +59,8 @@ MEDIA_URL = "/media/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''  # os.path.join(PROJECT_PATH, "static")
-#STATIC_ROOT = "/home/djaksont/domains/djakson.tk/public_html/static/"
+#STATIC_ROOT = ''  # os.path.join(PROJECT_PATH, "static")
+STATIC_ROOT = "/home/djaksont/domains/djakson.tk/public_html/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
