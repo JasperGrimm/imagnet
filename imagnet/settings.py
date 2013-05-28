5beff944465dbad41e75abb479599957fd8f76b5
angry_freelancer <eugenolshevsky@gmail.com>
2013-05-28 03:57:38 +0300

    change settings

--- imagnet/settings.py
+++ imagnet/settings.py
@@ -59,8 +59,8 @@ MEDIA_URL = "/media/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
-STATIC_ROOT = "/home/djaksont/domains/djakson.tk/public_html/static/"
+STATIC_ROOT = ''  # os.path.join(PROJECT_PATH, "static")
+#STATIC_ROOT = "/home/djaksont/domains/djakson.tk/public_html/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
@@ -68,7 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # os.path.join(PROJECT_PATH, "static"),
+    os.path.join(PROJECT_PATH, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
